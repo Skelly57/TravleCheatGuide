5a82955609a8159cb10478974eadaaf06d57f547
Skelly57 <92829191+Skelly57@users.noreply.github.com>
2024-04-29 16:32:07 -0400

    TravleCheatGuide 1.2
    -updated country_borders.txt to be in line w/ travle.earth
    -included 2 new functions, export_prev and import_prev
        -export_prev will take the matrix "prev" generated from the Floyd-Warshall Algorithm and push it into a text file
        -import_prev will take that text file and assign the values in it to the "prev" matrix
        -this is to improve performance on slower machines, no need to run the FW algo every launch, just run it once and import the rest
    -updated the switch statement to include new algorithms
    -included some error handling
        -misspelling a country will inform the user of their blunder
        -can't look up countries that start with X, as there are none

--- travle.cpp
+++ travle.cpp
@@ -13,6 +13,9 @@ void floyd_warshall(vector<vector<int>> &dist, vector<vector<int>> &prev);
 void shortest_path(string start_country, string end_country, vector<vector<int>> prev, vector<string> countries);
 int sp_recursion(int i, int j, vector<vector<int>> prev, vector<string> countries);
 void country_lookup(char letter, vector<string> countries);
+bool check_country(string country1, string country2, vector<string> countries);
+void import_prev(vector<vector<int>> &prev);
+void export_prev(vector<vector<int>> prev);
 
 int main()
 {
@@ -22,18 +25,20 @@ int main()
     vector<vector<int>> prev;
     countries_graph = import(countries);
     initialize(dist, prev, countries, countries_graph);
-    floyd_warshall(dist, prev);
     int x, loop = 1;
     string start_country, end_country;
     char letter;
     cout << "Welcome to the Travle Cheat Guide! Here you can find the shortest distance between two countries for all your travle.earth needs!\n"
-         << "To continue, enter one of the numbers below, depending on what you need :D"
+         << "To continue, enter one of the numbers below, depending on what you need :D\n"
          << "Also, for the country lookup, enter your letter as a capital please :)" << endl;
     while (loop == 1)
     {
         cout << "1. Find the Shortest Path between 2 Countries\n"
              << "2. Search for a country\n"
-             << "3. Exit the program" << endl;
+             << "3. Run the Floyd Warshall Algorithm\n"
+             << "4. Export the FW Matrix\n"
+             << "5. Import the FW Matrix\n"
+             << "6. Exit the program" << endl;
         cin >> x;
         switch (x)
         {
@@ -43,12 +48,36 @@ int main()
                 getline(cin, start_country); // it just works
                 cout << "Enter the End Country:" << endl;
                 getline(cin, end_country);
-                shortest_path(start_country, end_country, prev, countries);
+                if (check_country(start_country, end_country, countries))
+                {
+                    shortest_path(start_country, end_country, prev, countries);
+                }
+                else
+                {
+                    cout << "Country not found :(\n" 
+                         << "Try searching for a country to ensure spelling!" << endl;
+                }
                 break;
             case 2:
                 cout << "Enter the starting letter of the country you wish to find: ";
                 cin >> letter;
-                country_lookup(letter, countries);
+                if (letter != 'X')
+                {
+                    country_lookup(letter, countries);
+                }
+                else
+                {
+                    cout << "No country begins with the letter X!" << endl;
+                }
+                break;
+            case 3:
+                floyd_warshall(dist, prev);
+                break;
+            case 4:
+                export_prev(prev);
+                break;
+            case 5:
+                import_prev(prev);
                 break;
             default:
                 cout << "Thank you for using the Travle Cheat Guide, have an above average day :)" << endl;
@@ -94,18 +123,19 @@ vector<vector<string>> import(vector<string> & countries)
             countries_graph.push_back(temp_vec);
         }
     }
+    countries_file.close();
     return countries_graph;
 }
 
 void initialize(vector<vector<int>> & dist, vector<vector<int>> & prev, vector<string> countries, vector<vector<string>> countries_graph)
 {
     vector<int> temp_dist_vec, temp_prev_vec;
-    for (int i = 0; i < 197; i++)
+    for (int i = 0; i < countries.size(); i++)
     {
         temp_dist_vec.push_back(999999);
         temp_prev_vec.push_back(-1);
     }
-    for (int i = 0; i < 197; i++)
+    for (int i = 0; i < countries.size(); i++)
     {
         dist.push_back(temp_dist_vec);
         prev.push_back(temp_prev_vec);
@@ -188,20 +218,97 @@ void country_lookup(char letter, vector<string> countries)
 {
     vector<int> c;
     cout << "Countries starting with " << letter << endl;
-    for (int i = 0; i < 196; i++)
+    for (int i = 0; i < countries.size(); i++)
     {
         if (countries[i][0] == letter)
         {
             c.push_back(i);
         }
     }
-    if (countries[196][0] == letter)
+    if (countries[countries.size()-1][0] == letter)
     {
-        c.push_back(196);
+        c.push_back(countries.size()-1);
     }
     for (int j = 0; j < c.size()-1; j++)
     {
         cout << countries[c[j]] << ", ";
     }
     cout << countries[c[c.size()-1]] << endl;
+}
+
+bool check_country(string country1, string country2, vector<string> countries)
+{
+    bool country1_found = false, country2_found = false;
+    for (int i = 0; i < countries.size(); i++)
+    {
+        if (countries[i] == country1)
+        {
+            country1_found = true;
+        }
+        if (countries[i] == country2)
+        {
+            country2_found = true;
+        }
+        if (country1_found == true && country2_found == true)
+        {
+            return true;
+        }
+    }
+}
+
+void import_prev(vector<vector<int>> &prev)
+{
+    string path = "prev.txt";
+    fstream prev_file;
+    prev_file.open(path, ios::in);
+    int i = 0;
+    if (!prev_file.is_open())
+    {
+        cout << "Could not open input file!" << endl;
+    }
+    else
+    {
+        string line;
+        while (getline(prev_file, line))
+        {
+            stringstream ss(line);
+            string token;
+            vector<string> tokens;
+            char delimiter = ',';
+            while (getline(ss, token, delimiter))
+            {
+                tokens.push_back(token);
+            }
+            for (int j = 0; j < prev[i].size(); j++)
+            {
+                prev[i][j] = stoi(tokens[j]);
+            }
+            i++;
+        }
+    }
+    prev_file.close();
+}
+
+void export_prev(vector<vector<int>> prev)
+{
+    string path = "prev.txt";
+    fstream prev_file;
+    prev_file.open(path, ios::out);
+
+    if (!prev_file.is_open())
+    {
+        cout << "Cout not open output file!" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < prev.size(); i++)
+        {
+            for (int j = 0; j < prev[i].size()-1; j++)
+            {
+                prev_file << prev[i][j] << ',';
+            }
+            prev_file << prev[i][prev[i].size()-1] << endl;
+        }
+    }
+    prev_file.close();
 }
